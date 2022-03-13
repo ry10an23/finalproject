7b7d2669d51b265cc9977d11491e3585b0b410d3
ry10an23 <kawaguchi.r823@gmail.com>
2022-03-12 21:41:18 -0800

    modal box

--- final_project.js
+++ final_project.js
@@ -1,11 +1,37 @@
-let regiBtn = document.getElementsByTagName("button")[0];
-let regiModal = document.getElementsByClassName("regiModal");
-
+let loginBtn = document.getElementsByTagName("button")[1];
+let loginModal = document.querySelectorAll("div.loginModal")[0];
 window.addEventListener("click", (event) => {
-    if(event.target == regiBtn) {
-        regiBtn.style.display = "none";
+    if(event.target == loginModal) {
+        loginModal.style.display = "none";
     }
 })
+loginBtn.addEventListener("click",()=>{
+    loginModal.style.display = "block";
+})
+
+
+let regiBtn = document.getElementsByTagName("button")[0];
+let regiModal = document.querySelectorAll("div.regiModal")[0];
+window.addEventListener("click", (event) => {
+    if(event.target == regiModal) {
+        regiModal.style.display = "none";
+    }
+})
+regiBtn.addEventListener("click",()=>{
+    regiModal.style.display = "block";
+})
+
+
+
+
+
+
+
+
+
+
+
+
 
 // let loginBtn = document.getElementsByTagName("button")[1];
 // let loginModal = document.getElementsByClassName("loginModal");
