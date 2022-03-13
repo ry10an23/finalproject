6ea7e7b6083ef18970d31eda119e64454695ed6b
ry10an23 <kawaguchi.r823@gmail.com>
2022-03-12 22:38:10 -0800

    add parent class and childs class

--- final_project.js
+++ final_project.js
@@ -55,16 +55,110 @@ for(let i=0;i<radioBtns.length;i++){
     })
 }
 
+//************** Students Class (PARENT) **************
+let engClass = ["ESL", "TOFEL", "IELTS"];
+let careerClass = ["WD", "UI", "UX", "HM", "DM" ];
 
+class Students {
+    constructor(id, name, email, password ,gpa) {
+        this.id = id;
+        this.name= name;
+        this.email = email;
+        this.password = password;
+        this.gpa = gpa;
+    }
+    
+    get ID() {
+        return this.id
+    }
+    set ID(newID) {
+        this.id = newID
+    }
 
+    get Name() {
+        return this.name;
+    }
+    set Name(newName) {
+        this.name = newName;
+    }
 
+    get Email() {
+        return this.email;
+    }
+    set Email(newEmail) {
+        this.email = newEmail;
+    }
 
+    get Password() {
+        return this.password
+    }
+    set Password(newPassword) {
+        this.password = newPassword
+    }
 
+    get GPA() {
+        return this.gpa
+    }
+    set GPA(newGPA) {
+        this.gpa = newGPA
+    }
+}
 
+//************** EslStudents Class (CHILD) **************
+class EslStudents extends Students {
+    constructor(id, name, email, password, courseNumber, courseCollection ,gpa) {
+        super(id, name, email, password ,gpa);
+        this.courseNumber = courseNumber;
+        this.courseCollection = courseCollection;
+    }
+    get CourseNumber() {
+        return this.courseNumber;
+    }
+    set CourseNumber(newCourseNumber) {
+        this.courseNumber = newCourseNumber;
+    }
+    get CourseCollection() {
+        return this.courseCollection;
+    }
+    set CourseCollection(newCourseCollection) {
+        this.courseCollection = newCourseCollection;
+    }
+    getGPA() {
 
+    }
+    setCourse() {
 
+    }
+    avgGPA() {
 
+    }
+}
+//************** CareerStudents Class (CHILD) **************
+class CareerStudents extends Students {
+    constructor(id, name, email, password, courseNumber, courseCollection ,gpa) {
+        super(id, name, email, password ,gpa);
+        this.courseNumber = courseNumber;
+        this.courseCollection = courseCollection;
+    }
+    get CourseNumber() {
+        return this.courseNumber;
+    }
+    set CourseNumber(newCourseNumber) {
+        this.courseNumber = newCourseNumber;
+    }
+    get CourseCollection() {
+        return this.courseCollection;
+    }
+    set CourseCollection(newCourseCollection) {
+        this.courseCollection = newCourseCollection;
+    }
+    getGPA() {
 
+    }
+    setCourse() {
 
-// let loginBtn = document.getElementsByTagName("button")[1];
-// let loginModal = document.getElementsByClassName("loginModal");
+    }
+    avgGPA() {
+        
+    }
+}
